b10a29b6c66300ca7c4ebb2ae8840cf0bbf8a1e7
Boriska <boristheblade.glebov@yandex.ru>
2023-12-28 22:21:54 +0300

    HomeWork Module28 done

---
+++ Module28/01_work_with_file_2/main.py
@@ -0,0 +1,30 @@
+class File:
+    """
+    Контекст менеджер File\n
+    - теперь при попытке открыть несуществующий файл менеджер автоматически создаёт и открывает этот файл в режиме записи;\n
+    - на выходе из менеджера подавляются все исключения,связанные с файлами.
+    """
+
+    def __init__(self, path: str, mode: str, encoding: str = 'utf8') -> None:
+        self.filename = path
+        self.mode = mode
+        self.encoding = encoding
+
+    # Не понимаю что здесь возвращает функция
+    def __enter__(self):
+        self.file = open(self.filename, self.mode, encoding=self.encoding)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.file.close()
+
+
+print('Задача 1. Работа с файлом 2')
+
+with File('example.txt', 'w+') as file:
+    file.write('Привет, человек\n')
+    # file.write(12)
+    file.write('Добрый день\n')
+    file.write('Земля наша планета\n')
+
+# ошибка не выдается, но если ставить ошибочное сообщение, то дальше программа ничего не делает
---
+++ Module28/04_request_caching/main.py
@@ -0,0 +1,89 @@
+from typing import Any, Callable
+
+
+class LRUCache:
+    """
+    Класс LRU Cache (Least Recently Used Cache),
+    который будет хранить ограниченное количество
+    запросов и автоматически удалять самые старые
+    при достижении лимита.
+    """
+
+    def __init__(self, capacity: int) -> None:
+        self._capacity = capacity
+        self._cache_dict = dict()
+        self.use_counter = dict()
+
+    @property
+    def capacity(self) -> int:
+        """Геттер максимального количества элементов Кэша"""
+        return self._capacity
+
+    @capacity.setter
+    def capacity(self, capacity: int) -> None:
+        """Сеттер максимального количества элементов Кэша"""
+        self._capacity = capacity
+
+    @property
+    def cache(self) -> str:
+        """Геттер - этот метод должен возвращать самый старый элемент"""
+        return (f' {list(self._cache_dict.keys())[0]} - '
+                f'{self._cache_dict[list(self._cache_dict.keys())[0]]}')
+
+    @cache.setter
+    def cache(self, new_elem: tuple) -> None:
+        """
+        Сеттер этот метод должен добавлять новый элемент и
+        удаляет наиболее старый элемент или менее используемый
+        """
+        if len(self._cache_dict.keys()) < self.capacity:
+            self._cache_dict[new_elem[0]] = new_elem[1]
+        else:
+            for k, v in self.use_counter.items():
+                if v == max(self.use_counter.values()):
+                    for key, value in self._cache_dict.items():
+                        if k != key:
+                            self._cache_dict.pop(key)
+                            if key in self.use_counter.keys():
+                                self.use_counter.pop(key)
+                            self._cache_dict[new_elem[0]] = new_elem[1]
+                            break
+
+    def print_cache(self) -> None:
+        """Выводит текущий кэш"""
+        print(', '.join(f'{k}:{v}' for k, v in self._cache_dict.items()))
+
+    def get(self, key) -> Any:
+        """Получаем значение по ключу"""
+        if key not in self.use_counter.keys():
+            self.use_counter[key] = 1
+            return self._cache_dict[key]
+        else:
+            self.use_counter[key] += 1
+            return self._cache_dict[key]
+
+
+print('Задача 4. Кэширование запросов')
+
+# Создаем экземпляр класса LRU Cache с capacity = 3
+cache = LRUCache(3)
+
+# Добавляем элементы в кэш
+cache.cache = ("key1", "value1")
+cache.cache = ("key2", "value2")
+cache.cache = ("key3", "value3")
+
+# # # Выводим текущий кэш
+cache.print_cache()  # key1 : value1, key2 : value2, key3 : value3
+#
+# # Получаем значение по ключу
+print(cache.get("key2"))  # value2
+
+#
+# # Добавляем новый элемент, превышающий лимит capacity
+cache.cache = ("key4", "value4")
+#
+# # Выводим обновленный кэш
+cache.print_cache()  # key2 : value2, key3 : value3, key4 : value4
+
+
